452ae08c39f8f57d947215ea62a77fd7a84c025b
Abdur Rozak <abdurrozak.skom@gmail.com>
2024-06-04 05:25:55 +0700

    Start
    Start

---
+++ header.php
@@ -0,0 +1,34 @@
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>AdminLTE v4 | Dashboard</title><!--begin::Primary Meta Tags-->
+  <meta name="title" content="AdminLTE v4 | Dashboard">
+  <meta name="author" content="ColorlibHQ">
+  <meta name="description" content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
+  <meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard">
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
---
+++ pages/faq.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Header -->
+		<?php include 'header.php';?>
+		<!-- End Header -->
+	</head>
+	
+	<!-- Credit -->
+	<?php include 'jack.php';?>
+	<!-- End Credit -->
+
+	<body class="hold-transition sidebar-mini layout-fixed">
+		<!-- Wrapper -->
+		<div class="wrapper">
+			<!-- Preloader -->
+			<?php include 'preloader.php';?>
+			<!-- End Preloader -->
+			
+			<!-- Navbar -->
+			<?php include 'navbar.php';?>
+			<!-- End Navbar -->
+
+			<!-- Main Sidebar Container -->
+			<aside class="main-sidebar sidebar-dark-primary elevation-4">
+				<!-- Brand Logo -->
+				<?php include 'brand.php';?>
+				<!-- End Brand Logo -->
+
+				<!-- Sidebar -->
+				<?php include 'sidebar.php';?>
+				<!-- End Sidebar -->
+			</aside>
+
+			<!-- Content-Wrapper -->
+			<div class="content-wrapper">
+				<?php include 'content-wrapper.php';?>
+
+				<!-- Content -->
+				
+
+                
+				<!-- End Content -->
+			</div>
+			<!-- End Content-Wrapper -->
+		</div>
+		<!-- End Wrapper -->
+	</body>
+
+	<!-- Footer -->
+	<?php include 'footer.php';?>
+	<!-- End Footer -->
+
+</html>
